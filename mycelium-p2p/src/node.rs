a28e72bcd2f49122f99fdbfe8a348a07437858e2
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2025-04-27 18:23:06 +0900

    refactor(node): move p2p logic to mycelium-p2p crate and clean up
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

---
+++ mycelium-p2p/src/node.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use libp2p::{
+    core::upgrade,
+    dns,
+    futures::StreamExt,
+    gossipsub::{
+        self, AllowAllSubscriptionFilter, Behaviour as GossipsubBehaviour, IdentTopic,
+        IdentityTransform, MessageAuthenticity, ValidationMode,
+    },
+    identity, noise,
+    swarm::SwarmBuilder,
+    tcp, websocket, yamux, Multiaddr, PeerId, Transport,
+};
+
+pub async fn start_p2p_node() -> Result<()> {
+    let id_keys = identity::Keypair::generate_ed25519();
+    let peer_id = PeerId::from(id_keys.public());
+    println!("Local peer id: {:?}", peer_id);
+
+    let transport = tcp::tokio::Transport::default()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise::Config::new(&id_keys)?)
+        .multiplex(yamux::Config::default())
+        .boxed();
+
+    let gossipsub_config = gossipsub::ConfigBuilder::default()
+        .validation_mode(ValidationMode::Permissive)
+        .build()
+        .expect("Valid config");
+
+    let behaviour = GossipsubBehaviour::<IdentityTransform, AllowAllSubscriptionFilter>::new(
+        MessageAuthenticity::Signed(id_keys),
+        gossipsub_config,
+    )
+    .expect("Correct config");
+
+    let mut swarm = SwarmBuilder::with_tokio_executor(transport, behaviour, peer_id).build();
+
+    loop {
+        tokio::select! {
+            event = swarm.select_next_some() => {
+                println!("Swarm Event: {:?}", event);
+            }
+        }
+    }
+}
