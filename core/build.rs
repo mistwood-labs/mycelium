f3edb58ea7c296539d0185549b07eb142496cc25
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2025-05-06 18:52:57 +0900

    chore(core): restore `Cargo.toml` and `src/lib.rs`
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- core/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    prost_build::Config::new()
-        .out_dir("src/proto")
-        .compile_protos(&["../proto/mycelium.proto"], &["../proto"])
-        .unwrap();
-}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,2 +1,6 @@
 pub mod api;
+// pub mod domain;
 mod frb_generated;
+// pub mod network;
+// pub mod storage;
+// pub mod util;
